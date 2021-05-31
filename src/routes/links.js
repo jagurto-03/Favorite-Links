6bfb52c7043d336d3950296a6d4a8ab9a270212f
jagurto-03 <jagurto.m03@gmail.com>
2021-05-31 02:45:03 -0500

    eliminando y editando registros con peticiones get y post

--- src/routes/links.js
+++ src/routes/links.js
@@ -23,4 +23,28 @@ router.get('/', async (req, res) =>{
     res.render('links/list', { links })
 })
 
+router.get('/delete/:id', async (req, res) =>{
+    const {id} = req.params
+    await pool.query('DELETE FROM links WHERE ID = ?', [id])
+    res.redirect('/links')
+})
+
+router.get('/edit/:id', async (req, res) => {
+    const { id } = req.params
+    const links = await pool.query('SELECT * FROM links WHERE ID = ?', [id])
+    res.render('links/edit', { link: links[0] })
+})
+
+router.post('/edit/:id', async (req, res) => {
+    const { id } = req.params
+    const { title, description, url } = req.body
+    const newLink = {
+        title,
+        description,
+        url
+    }
+    await pool.query('UPDATE links set ? WHERE id = ?',[newLink, id])
+    res.redirect('/links')
+})
+
 module.exports = router
