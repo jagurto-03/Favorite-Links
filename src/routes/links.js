7a781151b31be46e7c8bc9bcb4ed1897d7fbd36f
jagurto-03 <jagurto.m03@gmail.com>
2021-05-31 00:50:35 -0500

    listar links desde la base de datos con el metodo get

--- src/routes/links.js
+++ src/routes/links.js
@@ -15,7 +15,12 @@ router.post('/add', async (req, res) => {
         description
     }
     await pool.query('INSERT INTO links set ?', [newLink])
-    res.send('received')
+    res.redirect('/links')
+})
+
+router.get('/', async (req, res) =>{
+    const links = await pool.query('SELECT * FROM links')
+    res.render('links/list', { links })
 })
 
 module.exports = router
