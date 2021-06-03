c46f0cc9b556b494498b8d9c7ccfe927c63056a1
jagurto-03 <jagurto.m03@gmail.com>
2021-06-02 22:51:57 -0500

    registro de usuarios en mysql

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ const path = require('path')
 const flash = require('connect-flash')
 const session = require('express-session')
 const MySQLStore = require('express-mysql-session')
+const passport = require('passport')
+
 const { database } = require('./keys')
 
 //Initializations
 const app = express()
+require('./lib/passport')
 
 //Settings
 app.set('port', process.env.PORT || 4000)
@@ -33,6 +36,8 @@ app.use(flash())
 app.use(morgan('dev'))
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
+app.use(passport.initialize())
+app.use(passport.session())
 
 
 //Global Variables
